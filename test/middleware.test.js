58d65a9dafe3c06505a174d625c7943f96539460
AndrewGHC <drewwalsh1@gmail.com>
2017-03-14 03:51:29 +0200

    Initial commit

---
+++ test/middleware.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const githubWebhookApi = require('../index');
+
+describe('github-webhook-api', function() {
+  describe('#config()', function() {
+    it('Throws TypeError if a config object is not passed', function() {
+      expect(githubWebhookApi).to.throw(TypeError);
+    });
+    it('Throws TypeError if secret is not a string', function() {
+      expect(() => githubWebhookApi({ secret: 1, whitelistEvents: [] })).to.throw(TypeError);
+    });
+    it('Throws TypeError if whitelistEvents is not an array', function() {
+      expect(() => githubWebhookApi({ secret: '', whitelistEvents: 1 })).to.throw(TypeError);
+    });
+
+  });
+});
